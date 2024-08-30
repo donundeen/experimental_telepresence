5f6913921780cb95de3129e52a3068cecc27b719
don undeen <donundeen@gmail.com>
2024-08-30 13:41:23 -0400

    new code

--- experimental_telepresence/experimental_telepresence.ino
+++ experimental_telepresence/experimental_telepresence.ino
@@ -2,19 +2,87 @@
 CONFIGS HERE
 ********************/
 char* my_device_name = "lester";
-
+// Adafruit IO Account Configuration
+// (to obtain these values, visit https://io.adafruit.com and click on Active Key)
+#define AIO_USERNAME  "donundeenlcc"
+#define AIO_KEY       "[NO_SECRETS]]"
 
 
 
 /// set up WIFIManager
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <WiFiManager.h>          // install https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 WiFiManager wifiManager;
+#include "WiFiClientSecure.h"
+
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+// TIMING INCLUDES
+#include <AsyncTimer.h> //https://github.com/Aasim-A/AsyncTimer
+AsyncTimer t;
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+
+// Using port 8883 for MQTTS
+#define AIO_SERVERPORT  8883
 
 
+/*********************************
+DEVICE SETUPS
+*******************************/
+const int PIEZO_PIN = A2; // Piezo output
 
 
+/************ Global State (you don't need to change this!) ******************/
 
+// WiFiFlientSecure for SSL/TLS support
+WiFiClientSecure client;
 
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Subscribe sub_tapping = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/tapping");
+
+// io.adafruit.com root CA
+const char* adafruitio_root_ca = \
+      "-----BEGIN CERTIFICATE-----\n"
+      "MIIEjTCCA3WgAwIBAgIQDQd4KhM/xvmlcpbhMf/ReTANBgkqhkiG9w0BAQsFADBh\n"
+      "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+      "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n"
+      "MjAeFw0xNzExMDIxMjIzMzdaFw0yNzExMDIxMjIzMzdaMGAxCzAJBgNVBAYTAlVT\n"
+      "MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j\n"
+      "b20xHzAdBgNVBAMTFkdlb1RydXN0IFRMUyBSU0EgQ0EgRzEwggEiMA0GCSqGSIb3\n"
+      "DQEBAQUAA4IBDwAwggEKAoIBAQC+F+jsvikKy/65LWEx/TMkCDIuWegh1Ngwvm4Q\n"
+      "yISgP7oU5d79eoySG3vOhC3w/3jEMuipoH1fBtp7m0tTpsYbAhch4XA7rfuD6whU\n"
+      "gajeErLVxoiWMPkC/DnUvbgi74BJmdBiuGHQSd7LwsuXpTEGG9fYXcbTVN5SATYq\n"
+      "DfbexbYxTMwVJWoVb6lrBEgM3gBBqiiAiy800xu1Nq07JdCIQkBsNpFtZbIZhsDS\n"
+      "fzlGWP4wEmBQ3O67c+ZXkFr2DcrXBEtHam80Gp2SNhou2U5U7UesDL/xgLK6/0d7\n"
+      "6TnEVMSUVJkZ8VeZr+IUIlvoLrtjLbqugb0T3OYXW+CQU0kBAgMBAAGjggFAMIIB\n"
+      "PDAdBgNVHQ4EFgQUlE/UXYvkpOKmgP792PkA76O+AlcwHwYDVR0jBBgwFoAUTiJU\n"
+      "IBiV5uNu5g/6+rkS7QYXjzkwDgYDVR0PAQH/BAQDAgGGMB0GA1UdJQQWMBQGCCsG\n"
+      "AQUFBwMBBggrBgEFBQcDAjASBgNVHRMBAf8ECDAGAQH/AgEAMDQGCCsGAQUFBwEB\n"
+      "BCgwJjAkBggrBgEFBQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMEIGA1Ud\n"
+      "HwQ7MDkwN6A1oDOGMWh0dHA6Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEds\n"
+      "b2JhbFJvb3RHMi5jcmwwPQYDVR0gBDYwNDAyBgRVHSAAMCowKAYIKwYBBQUHAgEW\n"
+      "HGh0dHBzOi8vd3d3LmRpZ2ljZXJ0LmNvbS9DUFMwDQYJKoZIhvcNAQELBQADggEB\n"
+      "AIIcBDqC6cWpyGUSXAjjAcYwsK4iiGF7KweG97i1RJz1kwZhRoo6orU1JtBYnjzB\n"
+      "c4+/sXmnHJk3mlPyL1xuIAt9sMeC7+vreRIF5wFBC0MCN5sbHwhNN1JzKbifNeP5\n"
+      "ozpZdQFmkCo+neBiKR6HqIA+LMTMCMMuv2khGGuPHmtDze4GmEGZtYLyF8EQpa5Y\n"
+      "jPuV6k2Cr/N3XxFpT3hRpt/3usU/Zb9wfKPtWpoznZ4/44c1p9rzFcZYrWkj3A+7\n"
+      "TNBJE0GmP2fhXhP1D/XVfIW/h0yCJGEiV9Glm/uGOa3DXHlmbAcxSyCRraG+ZBkA\n"
+      "7h4SeM6Y8l/7MBRpPCz6l8Y=\n"
+      "-----END CERTIFICATE-----\n";
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'pub_tapping' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish pub_tapping = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/tapping");
+
+/*************************** Sketch Code ************************************/
 
 void setup() 
 {
@@ -22,13 +90,110 @@ void setup()
   Serial.println("connecting?");
   wifiManager.autoConnect(my_device_name);
   Serial.println("connected I think");
+  // Set Adafruit IO's root CA
+  client.setCACert(adafruitio_root_ca);
+  led_flash(LED_BUILTIN, 500, 250, 5);
+  setup_subscribers();  
+  setup_timers();
 }
+void setup_subscribers(){
+    // Setup MQTT subscription for onoff & slider feed.
+  mqtt.subscribe(&sub_tapping);
+}
+void setup_timers(){
+  t.setInterval(keepMQTTAlive, 500);
+//  t.setInterval(getPiezo, 100);
+//  t.setInterval(sendPiezo, 2000);
+}
+
+uint32_t maxPiezoThisCycle;
+uint32_t lastPiezoSent;
+
+void getPiezo(){
+  // Now we can publish stuff!
+  uint32_t piezoADC = analogRead(PIEZO_PIN);
+  float piezoV = piezoADC / 1023.0 * 5.0;
+  if(piezoADC > maxPiezoThisCycle){
+    maxPiezoThisCycle = piezoADC;
+  }
+
+}
+
+void sendPiezo(){
+
+  if(lastPiezoSent != maxPiezoThisCycle){
+    Serial.print(F("\nSending val "));
+    Serial.print(maxPiezoThisCycle);
+    Serial.print(F(" to test feed..."));
+    if (! pub_tapping.publish(maxPiezoThisCycle)) {
+      Serial.println(F("Failed"));
+    } else {
+      Serial.println(F("OK!"));
+    }
+  }
+  lastPiezoSent = maxPiezoThisCycle;
+
+  maxPiezoThisCycle = 0;  
+}
+
+void keepMQTTAlive(){
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();  
+
+  // ping the server to keep the mqtt connection alive
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+}
+
 
 void loop() 
 { 
-  delay(100);//delay 200ms
+  t.handle();
+  subscription_loop();
 }
 
+
+void subscription_loop(){
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    // Check if its the onoff button feed
+    if (subscription == &sub_tapping) {
+      Serial.print(F("sub_tapping: "));
+      Serial.println((char *)sub_tapping.lastread);
+    }
+  }
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+
+  Serial.println("MQTT Connected!");
+}
 
 void led_flash(int pin, int onms, int offms, int times){
   // flash an led a certain number of times. Good for status things.
